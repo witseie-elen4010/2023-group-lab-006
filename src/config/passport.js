7b1a0f8d528ef6835f0360c522d25269a4a8a748
ReubenE <zoolander58@yahoo.com>
2023-05-18 02:04:14 +0200

    Add basic login,welcome and dashboard pages

---
+++ src/config/passport.js
@@ -0,0 +1,46 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require("../models/user");
+
+module.exports = function(passport) {
+    passport.use(
+      new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+        // match user
+        User.findOne({ email: email })
+          .then((user) => {
+            if (!user) {
+              return done(null, false, { message: 'That email is not registered' });
+            }
+            // match password
+            bcrypt.compare(password, user.password)
+              .then((isMatch) => {
+                if (isMatch) {
+                  return done(null, user);
+                } else {
+                  return done(null, false, { message: 'Incorrect password' });
+                }
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+    );
+  
+    passport.serializeUser(function(user, done) {
+      done(null, user.id);
+    });
+  
+    passport.deserializeUser(function(id, done) {
+      User.findById(id)
+        .then((user) => {
+          done(null, user);
+        })
+        .catch((err) => {
+          done(err, null);
+        });
+    });
+  };
