f60a2c2f1eaa6368ed4b96f43fcdf47bcb28d7aa
ReubenE <zoolander58@yahoo.com>
2023-05-24 23:21:10 +0200

    Added seperate login and dashboard for Lecturers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const {ensureAuthenticated} = require("../config/auth.js")
-//login page
+//welcome page
 router.get('/', (req,res)=>{
     res.render('welcome');
 })
@@ -12,6 +12,12 @@ router.get('/register', (req,res)=>{
 
 router.get('/dashboard',ensureAuthenticated,(req,res)=>{
     res.render('dashboard',{
+        user: req.user
+        });
+})
+
+router.get('/dashboardLect',ensureAuthenticated,(req,res)=>{
+    res.render('dashboardLect',{
         user: req.user
         });
 })
