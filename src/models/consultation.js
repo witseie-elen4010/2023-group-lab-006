88f2a68f242a968f8588123fd3a396c27773521d
Maria Daras <darasmaria@gmail.com>
2023-05-25 16:10:02 +0200

    Database created which holds all of the student's information

---
+++ src/models/consultation.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const consultSchema = new mongoose.Schema({
+    lecturer: {
+        type: String,
+        required: true
+    },
+    consultDay: {
+        type: Date,
+        required: true
+    },
+    consultLength: {
+        type: Number,
+        default: 1,
+        required: true
+    },
+    consultStart: {
+        type: String,
+        required: true
+    }
+});
+const consultation = mongoose.model('consultation', consultSchema);
+
+module.exports = consultation;
---
+++ src/routes/consult.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const consult = require('../models/consultation');
+
+const saveConsult = newConsult.save()
+module.exports = router;
