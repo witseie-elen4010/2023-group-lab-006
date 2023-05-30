02779f2c9cfc6bc642df3c3015388eede5fbf278
Mannan2105 <110164989+Mannan2105@users.noreply.github.com>
2023-05-30 19:04:10 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,46 +1,54 @@
 // APP.JS in ~/app.js
-const express = require('express')
-const mongoose = require('mongoose')
-const router = express.Router()
-const app = express()
-const expressEjsLayout = require('express-ejs-layouts')
-const flash = require('connect-flash')
-const session = require('express-session')
-const passport = require('passport')
+const path = require('path'); // Add this line
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const app = express();
+const expressEjsLayout = require('express-ejs-layouts');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 
-require('./config/passport')(passport)
+require('./config/passport')(passport);
+
 // mongoose
 mongoose.connect('mongodb://127.0.0.1:/test', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('connected,,'))
-  .catch((err) => console.log(err))
+  .catch((err) => console.log(err));
 
 // EJS
-app.set('view engine', 'ejs')
-app.use(expressEjsLayout)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views')); // Set the path to the views directory
+app.use(expressEjsLayout);
+
 // BodyParser
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+
 // express session
 app.use(session({
   secret: 'secret',
   resave: true,
   saveUninitialized: true
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // use flash
-app.use(flash())
+app.use(flash());
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success_msg')
-  res.locals.error_msg = req.flash('error_msg')
-  res.locals.error = req.flash('error')
-  next()
-})
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
 // Routes
-app.use('/', require('./routes/index'))
-app.use('/users', require('./routes/users'))
-app.use('/lectInfo', require('./routes/lectInfo'))
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+app.use('/lectInfo', require('./routes/lectInfo'));
 
 // app.listen(3000);
-const port = process.env.PORT || 3000
-app.listen(port)
-console.log('Express server running on port', port)
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Express server running on port', port);
