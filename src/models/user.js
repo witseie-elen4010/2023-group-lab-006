130826a4847f4f38d0dab2eb8ac5fab1bba0e264
Mannan Johari <joharimannan1@gmail.com>
2023-06-02 10:17:23 +0200

    final review,user story and scrumboard update

--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,28 @@
-//USER.JS in MODELS/USER.JS
-const mongoose = require('mongoose');
-const UserSchema  = new mongoose.Schema({
-  name :{
-      type  : String,
-      required : true
-  } ,
-  email :{
-    type  : String,
-    required : true
-} ,
-password :{
-    type  : String,
-    required : true
-} ,
-date :{
-    type : Date,
-    default : Date.now
-},
-type :{
-    type : String,
-    default : "user",
-    required : true
-}
-});
-const User= mongoose.model('User',UserSchema);
+// USER.JS in MODELS/USER.JS
+const mongoose = require('mongoose')
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  type: {
+    type: String,
+    default: 'user',
+    required: true
+  }
+})
+const User = mongoose.model('User', UserSchema)
 
-module.exports = User;
+module.exports = User
